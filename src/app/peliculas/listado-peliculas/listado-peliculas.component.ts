987f37c52e2e93ded4e192edfe8f43c91eb6daf8
jriscod <jordy.riscod@gmail.com>
2024-12-14 01:14:32 -0500

    se agrego angular material

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,12 +1,14 @@
 import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from "@angular/material/button";
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-listado-peliculas',
   standalone: true,
-  imports: [DatePipe, ListadoGenericoComponent, ListadoGenericoComponent, MatButtonModule],
+  imports: [DatePipe, ListadoGenericoComponent, ListadoGenericoComponent, MatButtonModule, MatIconModule],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
