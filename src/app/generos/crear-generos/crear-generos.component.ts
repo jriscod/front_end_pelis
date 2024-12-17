3d786d76b5aed4f10b2f6789c8c2d951463aebf3
jriscod <jordy.riscod@gmail.com>
2024-12-16 20:40:51 -0500

    formularios

--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,18 +1,26 @@
 import { Component, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import { Router } from '@angular/router';
+import { Router, RouterLink} from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { primeraLetraMayuscula } from '../../compartidos/componentes/funciones/validaciones';
+import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
+import { GeneroCreacionDTO } from '../generos';
+
 @Component({
   selector: 'app-crear-generos',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, MatFormFieldModule, ReactiveFormsModule, MatInputModule, FormularioGeneroComponent],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css'
 })
 export class CrearGenerosComponent {
- router = inject(Router);
-
-  guardarCambios(){
-    this.router.navigate(['/generos']);
+ private router = inject(Router);
+ 
+  guardarCambios(genero: GeneroCreacionDTO){
+    //this.router.navigate(['/generos']);
+    console.log('Creando el genero',genero);
   }
 
 }
