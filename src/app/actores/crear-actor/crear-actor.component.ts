80b8d20140dce084cfe8e2a55ee259caf89806f9
jriscod <jordy.riscod@gmail.com>
2025-02-04 13:01:28 -0500

    datapicker

--- src/app/actores/crear-actor/crear-actor.component.ts
+++ src/app/actores/crear-actor/crear-actor.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormularioActoresComponent } from "../formulario-actores/formulario-actores.component";
 
 @Component({
   selector: 'app-crear-actor',
   standalone: true,
-  imports: [],
+  imports: [FormularioActoresComponent],
   templateUrl: './crear-actor.component.html',
   styleUrl: './crear-actor.component.css'
 })
---
+++ src/app/actores/formulario-actores/formulario-actores.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-formulario-actores',
+  standalone: true,
+  imports: [MatButtonModule, RouterLink,MatFormFieldModule, ReactiveFormsModule,MatInputModule, MatDatepickerModule],
+  templateUrl: './formulario-actores.component.html',
+  styleUrl: './formulario-actores.component.css'
+})
+export class FormularioActoresComponent {
+    private formBuilder = inject(FormBuilder);
+
+    form = this.formBuilder.group({
+      nombre: ['', {
+        validators: [Validators.required]
+      }],
+      fechaNacimiento: new FormControl<Date | null>(null)
+    })
+}
